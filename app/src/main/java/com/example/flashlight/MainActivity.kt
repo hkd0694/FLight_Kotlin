d518f9f02a75dcd65a2fcae43acbdf598a688dd0
hkd0694 <gksrudehd123@naver.com>
2019-11-08 16:10:14 +0900

    Service 추가

--- app/src/main/java/com/example/flashlight/MainActivity.kt
+++ app/src/main/java/com/example/flashlight/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.flashlight
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.intentFor
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,15 +11,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val torch = Torch(this)
+        //val torch = Torch(this)
 
-        flashSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+        flashSwitch.setOnCheckedChangeListener { _, isChecked ->
             if(isChecked){
-                torch.flashOn()
+                startService(intentFor<TorchService>().setAction("on"))
             } else{
-                torch.flashOff()
+                startService(intentFor<TorchService>().setAction("off"))
             }
         }
-
     }
 }
--- app/src/main/java/com/example/flashlight/Torch.kt
+++ app/src/main/java/com/example/flashlight/Torch.kt
@@ -7,6 +7,10 @@ import android.hardware.camera2.CameraManager
 class Torch(context: Context) {
 
     private var cameraId: String? = null
+
+    //context의 getSystemService() 메서드는 안드로이드 시스템에서 제공하는 각종 서비스를 관리하는 매너저 클래스를 생성.
+    //인자로 Context 클래스에 정의된 서비스를 정의한 상수를 지정한다. 이 메서드는 Object형을 반환하기 때문에
+    // as 연산자를 사용하여 CameraService형으로 형변환을 한다.
     private val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
     init{
@@ -24,16 +28,18 @@ class Torch(context: Context) {
     private fun getCameraId(): String? {
         val cameraIds = cameraManager.cameraIdList
         for(id in cameraIds){
+            //각 ID 별로 세부 정보를 가지는 객체
             val info = cameraManager.getCameraCharacteristics(id)
+            //플래시 가능 여부
             val flashAvailable = info.get(CameraCharacteristics.FLASH_INFO_AVAILABLE)
+            //카메라의 렌즈 방향
             val lensFacing = info.get(CameraCharacteristics.LENS_FACING)
+            
+            //플래시가 사용 가능하고 카메라가 기기의 뒷면을 향하고 있는 카메라의 ID를 찾는다면 id 반환 아니면 null 반환
             if(flashAvailable != null && flashAvailable && lensFacing != null && lensFacing == CameraCharacteristics.LENS_FACING_BACK){
                 return id
             }
         }
         return null
     }
-
-
-
 }
